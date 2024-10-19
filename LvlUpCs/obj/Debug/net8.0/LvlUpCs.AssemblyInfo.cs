9ff54fea694141f0788beae01a5435a2e00fee11
Brycenjwill <114171494+Brycenjwill@users.noreply.github.com>
2024-10-19 14:11:21 -0600

    Added template for 3 tasks

--- LvlUpCs/obj/Debug/net8.0/LvlUpCs.AssemblyInfo.cs
+++ LvlUpCs/obj/Debug/net8.0/LvlUpCs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LvlUpCs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fc73666146dd694a99f406dd6c5aa9a281f6580")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e848781001c8ddd19d3b08dfa166dc4d5f5b4905")]
 [assembly: System.Reflection.AssemblyProductAttribute("LvlUpCs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LvlUpCs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- lvlfrontend/src/assets/carousel.jsx
+++ lvlfrontend/src/assets/carousel.jsx
@@ -13,16 +13,27 @@ function ControlledCarousel() {
   return (
     <Carousel activeIndex={index} onSelect={handleSelect} interval={null} pause={null}>
       <Carousel.Item>
-      <div className="d-block w-100">
-          <h3>Physo</h3>
+      <div id="tree">
+      <div id="str1">text</div>
+        <div id="str2">text</div>
+        <div id="str3">text</div>
       </div>
       </Carousel.Item>
       <Carousel.Item>
-
+      <div id="tree">
+      <div id="int1">text2</div>
+        <div id="int2">text2</div>
+        <div id="int3">text2</div>
+      </div>
 
       </Carousel.Item>
       <Carousel.Item>
+      <div id="tree">
 
+        <div id="soc1">text3</div>
+        <div id="soc2">text3</div>
+        <div id="soc3">text3</div>
+      </div>
 
       </Carousel.Item>
     </Carousel>
