2c9b61fb5a0e3aa18102092a2485c2cd0d3171a3
j0shu1 <joh21036@byui.edu>
2024-10-18 20:50:17 -0600

    loosened .gitignore

--- LvlMeUp/LvlMeUp.Server/obj/Release/net8.0/LvlMeUp.Server.AssemblyInfo.cs
+++ LvlMeUp/LvlMeUp.Server/obj/Release/net8.0/LvlMeUp.Server.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("LvlMeUp.Server")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c8f9109c935170c2cca15ea1c3774e4f23ce58e")]
 [assembly: System.Reflection.AssemblyProductAttribute("LvlMeUp.Server")]
